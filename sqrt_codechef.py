e8fd9e380988d392dc486deff116c802fe953fc5
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2022-09-30 23:10:31 +0530

    Add files via upload

---
+++ sqrt_codechef.py
@@ -0,0 +1,15 @@
+"""
+finding the square root in python by codechef
+"""
+
+
+# def sqrt(a):
+#     ans=1 
+#     while (ans*ans)<=a:
+#         ans+=1
+#     return ans-1    
+# n=int(input())
+# for i in range(n):
+#     num=int(input())
+#     root=sqrt(num)
+#     print(root)
